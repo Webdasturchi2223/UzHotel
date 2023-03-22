e744d289f2591d4fe6aeba63d6b0b2a2a0623ef3
Webdasturchi2223 <webdasturchi2223@gmail.com>
2023-03-22 10:27:15 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 100) {
 			$('.nav').fadeIn();
+			$('.scroll').fadeIn();
 		} else {
 			$('.nav').fadeOut();
+			$('.scroll').fadeOut();
 		}
+	});
+	$('.scroll').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 300);
+		return false;
 	});
 });
